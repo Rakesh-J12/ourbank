6f97e5b2e93417a83a8e58f2f012aada48ebfebd
Rakesh Jangde <44526632+Rakesh-J12@users.noreply.github.com>
2020-12-15 13:44:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rakesh <code>src/App.js</code> and save to reload.
-        </p>
-        This is dummy update.
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hi Rakesh</h1>
       </header>
     </div>
   );
